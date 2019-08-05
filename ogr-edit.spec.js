a0a82d59bcf1652e42cc1dedc8b1ea8b46e10ac3
nto4 <mehmet94nto@gmail.com>
2019-08-05 13:13:04 +0300

    onClickEvent funciton added

---
+++ ogr-edit.spec.js
@@ -0,0 +1,9 @@
+describe('Type Describe', function (){
+	it('Type fetch details', async function () {
+ 	browser.waitForAngular
+		await browser.get('http://localhost:TypeHere/');
+
+		await expect(browser.getTitle()).toEqual('TypeHere');
+
+	});
+});
